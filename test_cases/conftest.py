db09e6d08444121d5ea1965717a20584e7b5b495
Ilya Rahmilevich <110979314+ilyar88@users.noreply.github.com>
2025-01-09 21:05:09 +0200

    Update conftest.py

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -3,6 +3,9 @@ import allure
 import pytest
 from applitools.selenium import Eyes
 from selenium import webdriver
+import appium.webdriver
+from appium.webdriver.common.multi_action import MultiAction
+from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.common.action_chains import ActionChains
 #from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,12 +17,15 @@ from utilities.common_ops import get_data, get_time_stamp
 
 driver = None
 action = None
+action2 = None #For using multiple actions
+m_action = None
+mobile_size = None
 eyes = Eyes()  # Applitools Eyes instance
 
 @pytest.fixture(scope='class')
 def init_web_driver(request):
     # Initialize the driver based on the browser setting
-    globals()['driver'] = get_web_driver(get_data('Browser'))
+    globals()['driver'] = get_web_driver()
     #globals()['driver'] = EventFiringWebDriver(edriver, EventListener())
     driver = globals()['driver']
     driver.maximize_window()
@@ -41,8 +47,26 @@ def current_page(request):
     yield
     driver.quit()
 
-def get_web_driver(browser_name):
-    browser_name = browser_name.lower()
+@pytest.fixture(scope='class')
+def init_mobile_driver(request):
+    globals()['driver'] = get_mobile_driver()
+    driver = globals()['driver']
+    driver.implicitly_wait(int(get_data('WaitTime')))
+    request.cls.driver = driver
+    globals()['action'] = TouchAction(driver)
+    request.cls.action = globals()['action']
+    globals()['action2'] = TouchAction(driver)
+    request.cls.action2 = globals()['action2']
+    globals()['m_action'] = MultiAction(driver)
+    request.cls.m_action = globals()['m_action']
+    globals()['mobile_size'] = driver.get_window_size()
+    request.cls.mobile_size = globals()['mobile_size']
+    ManagePages.init_mobile_pages()
+    yield
+    driver.quit()
+
+def get_web_driver():
+    browser_name = get_data('Browser').lower()
     if browser_name == 'chrome':
         return webdriver.Chrome(ChromeDriverManager().install())
     elif browser_name == 'firefox':
@@ -51,6 +75,34 @@ def get_web_driver(browser_name):
         return webdriver.Edge(EdgeChromiumDriverManager().install())
     else:
         raise ValueError(f"Invalid browser name: '{browser_name}'. Supported: 'Chrome', 'Firefox', 'Edge'.")
+
+def get_mobile_driver():
+    device_type = get_data('Mobile_device').lower()
+    if device_type == 'android':
+        return get_android()
+    elif device_type == 'ios':
+        return get_ios()
+    else:
+        raise ValueError(f"Wrong input, unrecognized  mobile OS.")
+
+def get_android():
+    dc = {
+        'udid': get_data('Udid'),
+        'appPackage': get_data('App_package'),
+        'appActivity': get_data('App_activity'),
+        'platformName': 'android'
+    }
+    android_driver = appium.webdriver.Remote(get_data('Appium_server'), dc)
+    return android_driver
+
+def get_ios():
+    dc = {
+        'udid': get_data('Udid'),
+        'bundle_id': get_data('Bundle_id'),
+        'platformName': 'ios'
+    }
+    ios_driver = appium.webdriver.Remote(get_data('Appium_server'), dc)
+    return ios_driver
 
 # Catch exceptions and errors to take screenshots on failures
 def pytest_exception_interact(node, call, report):
