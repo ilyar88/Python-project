b7168db51b6039bc8a78207a5510de58bc18229d
Ilya Rahmilevich <110979314+ilyar88@users.noreply.github.com>
2025-01-23 15:12:40 +0200

    Update common_ops.py

--- utilities/common_ops.py
+++ utilities/common_ops.py
@@ -7,9 +7,14 @@ import test_cases.conftest as conf
 import xml.etree.ElementTree as ET
 from enum import Enum
 
-
+##############################################################
+# Function ame: get_data
+# Function description: this function reads data from external file
+# Function parameters: string - node(tag) name
+# Function return: string - node(tag) value
+##############################################################
 def get_data(node_name):
-    root = ET.parse('../configuration/data.xml').getroot()
+    root = ET.parse('configuration/data.xml').getroot()
     return root.find('.//' + node_name).text
 
 def wait(for_element, locator: tuple[By, str]):
@@ -17,6 +22,8 @@ def wait(for_element, locator: tuple[By, str]):
         WebDriverWait(conf.driver, int(get_data('WaitTime'))).until(EC.presence_of_element_located(locator))
     elif for_element == For.ELEMENT_DISPLAYED:
         WebDriverWait(conf.driver, int(get_data('WaitTime'))).until(EC.visibility_of_element_located(locator))
+    elif for_element == For.ELEMENT_CLICKABLE:
+        WebDriverWait(conf.driver, int(get_data('WaitTime'))).until(EC.element_to_be_clickable(locator))
 
 def read_csv(file_name):
     data = []
@@ -33,6 +40,7 @@ def get_time_stamp():
 class For(str, Enum):
     ELEMENT_EXISTS = 'element_exists'
     ELEMENT_DISPLAYED = 'element_displayed'
+    ELEMENT_CLICKABLE = 'element_clickable'
 
 class By(str, Enum):
     USER = 'user'
@@ -48,3 +56,12 @@ class Direction(str, Enum):
     RIGHT = 'right'
     DOWN = 'down'
     UP = 'up'
+
+class RGB(str, Enum):
+    SALMON_RED = "rgb(255, 103, 93)"
+    ORANGE = "rgb(249, 167, 77)"
+    YELLOW = "rgb(245, 206, 83)"
+    GREEN = "rgb(114, 204, 87)"
+    BLUE = "rgb(87, 185, 244)"
+    PURPLE = "rgb(210, 137, 226)"
+    GRAY = "rgb(165, 165, 167)"
